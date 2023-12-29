49cd2df71b7d22910ee749664a50789bee8b5e5e
VjumpKunG <vjumpkung@gmail.com>
2023-12-29 13:52:46 +0700

    add api/v1 prefix in every endpoints

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableVersioning({
+    defaultVersion: '1',
     type: VersioningType.URI,
     prefix: 'api/v',
   });
